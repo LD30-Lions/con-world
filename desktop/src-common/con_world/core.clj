2d7d5b6532c5b906d0eda2c7b431669ed65b9f8f
Jérôme Prudent <jprudent@gmail.com>
2014-08-23 15:41:05 +0200

    added wall

--- desktop/src-common/con_world/core.clj
+++ desktop/src-common/con_world/core.clj
@@ -7,56 +7,75 @@
 
 (declare main-screen con-world)
 
-(def w-width (* 10 50))
+(def tile-width 50)
+(def w-width (* 10 tile-width))
+(def w-height (* 30 tile-width))
+
+(defn create-rect-body!
+  [screen width height]
+  (let [body (add-body! screen (body-def :static))]
+    (->> [0 0
+          0 height
+          width height
+          width 0
+          0 0]
+         float-array
+         (chain-shape :create-chain)
+         (fixture-def :density 1 :shape)
+         (body! body :create-fixture))
+    body))
 
 (defscreen main-screen
-  :on-show
-  (fn [screen _]
-    (let [screen (update! screen
-                          :camera (orthographic)
-                          :renderer (stage)
-                          :world (box-2d 0 0))]
+           :on-show
+           (fn [screen _]
+             (let [screen (update! screen
+                                   :camera (orthographic)
+                                   :renderer (stage)
+                                   :world (box-2d 0 0))
+                   wall (doto {:body (create-rect-body! screen w-width w-height)}
+                          (body-position! 0 0 0))]
 
-      (width! screen w-width)
-      [(doto (cell/create-cell-entity! screen)
-         (body-position! 30
-                         30
-                         0)
-         (body! :set-linear-velocity 10 10))]))
-  
-  :on-render
-  (fn [screen entities]
-    (clear!)
-    (->> entities
-         (step! screen)
-         (render! screen)))
+               (size! screen w-width w-height)
+               [wall
+                (doto (cell/create-cell-entity! screen)
+                  (body-position! 30
+                                  30
+                                  0)
+                  (body! :set-linear-velocity 100 100))]))
 
-  :on-key-down
-    (fn [screen entities]
-      (condp = (:key screen)
-        (key-code :dpad-up)
-        (replace {(cell/find-cell entities)
-                  (cell/move-cell entities :up)}
-                 entities)
-        (key-code :dpad-down)
-        (replace {(cell/find-cell entities)
-                   (cell/move-cell entities :down)}
-                 entities)
-        (key-code :dpad-left)
-        (replace {(cell/find-cell entities)
-                   (cell/move-cell entities :left)}
-                 entities)
-        (key-code :dpad-right)
-        (replace {(cell/find-cell entities)
-                   (cell/move-cell entities :right)}
-                 entities)
-        nil)
+           :on-render
+           (fn [screen entities]
+             (clear!)
+             (->> entities
+                  (step! screen)
+                  (render! screen)))
 
-      :on-resize
-        (fn [screen _]
-          (width! screen w-width))))
+           :on-key-down
+           (fn [screen entities]
+             (condp = (:key screen)
+               (key-code :dpad-up)
+               (replace {(cell/find-cell entities)
+                          (cell/move-cell entities :up)}
+                        entities)
+               (key-code :dpad-down)
+               (replace {(cell/find-cell entities)
+                          (cell/move-cell entities :down)}
+                        entities)
+               (key-code :dpad-left)
+               (replace {(cell/find-cell entities)
+                          (cell/move-cell entities :left)}
+                        entities)
+               (key-code :dpad-right)
+               (replace {(cell/find-cell entities)
+                          (cell/move-cell entities :right)}
+                        entities)
+               nil)
+
+             :on-resize
+             (fn [screen _]
+               (width! screen w-width))))
 
 (defgame con-world
-  :on-create
-  (fn [this]
-    (set-screen! this main-screen)))
+         :on-create
+         (fn [this]
+           (set-screen! this main-screen)))
