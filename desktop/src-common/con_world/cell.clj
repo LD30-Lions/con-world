402f1a0c77fc5db9a2d1e6ec4d78640f8d45a471
Jérôme Prudent <jprudent@gmail.com>
2014-08-24 03:00:56 +0200

    déplacement aléatoire des ennemis

--- desktop/src-common/con_world/cell.clj
+++ desktop/src-common/con_world/cell.clj
@@ -30,16 +30,51 @@
    :left  (vector-2 (- u/x-velocity) 0)
    :right (vector-2 u/x-velocity 0)})
 
-(defn change-velocity [entity direction]
-  (let [current-velocity (body! entity :get-linear-velocity)
+(defn change-cell-velocity [cell-entity direction]
+  (let [current-velocity (body! cell-entity :get-linear-velocity)
         new-velocity (direction direction->velocity)]
-    (body! entity
+    (body! cell-entity
            :set-linear-velocity (vector-2! current-velocity :add new-velocity))
+    cell-entity))
+
+(defn rand-sign [x]
+  (if (= 0 (mod (rand-int 2) 2)) (* -1 x) (* 1 x)))
+
+(defn change-enemy-velocity [enemy-entity]
+  (let [current-velocity (body! enemy-entity :get-linear-velocity)
+        new-velocity (vector-2 (rand-sign (rand-int u/x-velocity)) (rand-sign (rand-int u/y-velocity)))]
+    (body! enemy-entity
+           :set-linear-velocity (vector-2! current-velocity :add new-velocity))
+    enemy-entity))
+
+(defn moving-fast? [entity]
+  (let [vec2-velocity (body! entity :get-linear-velocity)
+        x-velocity (.x vec2-velocity)
+        y-velocity (.y vec2-velocity)]
+    (or (> x-velocity u/moving-slow) (> y-velocity u/moving-slow))))
+
+(defn move-enemy [{:keys [enemy?] :as entity}]
+  (if (and enemy? (not (moving-fast? entity)))
+    (change-enemy-velocity entity)
     entity))
+
+(defn in-rectangle? [{:keys [x y width height]} r]
+  (and (rectangle! r :contains x y) (rectangle! r :contains (+ x width) (+ y height))))
+
+(defn set-enemy-in-zone [{:keys [in-zone? enemy?] :as entity}]
+  (if (and (not in-zone?) enemy?)
+    (let [r (rectangle 0 0 u/w-width u/w-height)]
+      (if (in-rectangle? entity r)
+        (do
+          (println "in zone")
+          (assoc entity :in-zone? true))
+        entity))
+    entity))
+
 
 (defn move-cell [entities direction]
   (let [cell (some #(when (:cell? %) %) entities)]
-    (change-velocity cell direction)))
+    (change-cell-velocity cell direction)))
 
 (defn find-cell [entities]
   (some #(when (:cell? %) %) entities))
@@ -55,9 +90,9 @@
 
 (defn create-enemy-body!
   [screen radius]
-  (let [body (add-body! screen (body-def :dynamic :linear-damping 0))]
+  (let [body (add-body! screen (body-def :dynamic :linear-damping 3))]
     (->> (circle-shape radius)
-         (fixture-def :density 1 :friction 0 :restitution 1 :shape)
+         (fixture-def :density 1 :friction 0 :restitution 0.7 :shape)
          (body! body :create-fixture))
     body))
 
